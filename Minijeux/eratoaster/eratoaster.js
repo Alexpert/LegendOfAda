6955d1a27e75b52613ce8ae6a7532f872c746ba6
Alexpert <salmon.alexandre10@gmail.com>
2017-11-18 16:13:19 +0100

    kuyvf

--- Minijeux/eratoaster/eratoaster.js
+++ Minijeux/eratoaster/eratoaster.js
@@ -7,7 +7,7 @@ var toasts = [];
 var score = 0;
 var startTime;
 
-var gravity = 0.2;
+var gravity = 0.001 * windowHeight;
 var primeNumbers = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97];
 var probaToast = 0.02;
 var gain = 5;
@@ -39,7 +39,7 @@ function Eratoaster() {
 function Toast(x) {
   this.x = x;
   this.y = windowHeight - eratoasterSprite.height;
-  this.velocity = -(7 + Math.floor(Math.random() * 5));
+  this.velocity = -Math.floor(Math.random() * windowsHeight);
   this.value = Math.floor(Math.random() * 100);
   this.hit = false;
 
