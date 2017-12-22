034731f0e4a9dc6754d5741ae08e48c138887f2d
Valentin Debon <valentin@debon.net>
2017-12-22 21:36:47 +0100

    Plus de bd

--- Model/insert.sql
+++ Model/insert.sql
@@ -1,48 +1,77 @@
-insert into worlds values ('Mongolie');
-insert into worlds values ('France');
-insert into worlds values ('Italie');
-insert into worlds values ('Inde');
-insert into worlds values ('Atlantis');
-insert into worlds values ('Enfer');
+insert into worlds values
+	('Mongolie'),
+	('France'),
+	('Italie'),
+	('Inde'),
+	('Atlantis'),
+	('Enfer');
 
-insert into themes values (1, 'Nombre et calculs');
-insert into themes values (2, 'Organisation et gestion de données, fonctions');
-insert into themes values (3, 'Grandeurs et mesures');
-insert into themes values (4, 'Espace et géométrie');
-insert into themes values (5, 'Algorithmique et programmation');
+insert into themes (name) values
+	('Nombre et calculs'),
+	('Organisation et gestion de données, fonctions'),
+	('Grandeurs et mesures'),
+	('Espace et géométrie'),
+	('Algorithmique et programmation');
 
-insert into lessons values (1, 'Nombres premiers et fractions irréductibles', 1);
-insert into lessons values (2, 'Calcul littéral', 1);
-insert into lessons values (3, 'Equations et Inéquations', 1);
-insert into lessons values (4, 'Fonctions', 2);
-insert into lessons values (5, 'Fonctions linéaires, fonctions affines', 2);
-insert into lessons values (6, 'Proportionnalité', 2);
-insert into lessons values (7, 'Statistiques et probabilités', 2);
-insert into lessons values (8, 'Géométrie dans l''espace', 3);
-insert into lessons values (9, 'Les transformations du plan - Homothéties', 4);
-insert into lessons values (10, 'Théorème de Thalès', 4);
-insert into lessons values (11, 'Trigonométrie', 4);
-insert into lessons values (12, 'Algorithmique', 5);
-insert into lessons values (13, 'Programmation', 5);
+-- Par chance, SERIAL commence a 1
+insert into lessons (name, theme) values
+	('Nombres premiers et fractions irréductibles', 1),
+	('Calcul littéral', 1),
+	('Equations et Inéquations', 1),
+	('Fonctions', 2),
+	('Fonctions linéaires, fonctions affines', 2),
+	('Proportionnalité', 2),
+	('Statistiques et probabilités', 2),
+	('Géométrie dans l''espace', 3),
+	('Les transformations du plan - Homothéties', 4),
+	('Théorème de Thalès', 4),
+	('Trigonométrie', 4),
+	('Algorithmique', 5),
+	('Programmation', 5);
 
-insert into games (name, about) values ('Eratoaster', 1);
-insert into games (name, about) values ('Identités remarquables', 1);
+insert into games (name, about) values
+	('Eratoaster', 1);
 
-insert into achievements (name, description) values ('Un début à tout', 'Créer un compte');
-insert into achievements (name, description) values ('Chef de guilde', 'Créer une guilde');
-insert into achievements (name, description) values ('Apprenti sauveur', 'Vous avez sauvé Ada Lovelace');
-insert into achievements (name, description) values ('Baguette vendéenne', 'Vous avez sauvé François Viète');
-insert into achievements (name, description) values ('Veni Vidi Zombi', 'Vous avez sauvé Maria Gaëtana Agnési');
-insert into achievements (name, description) values ('Un curry assez épicé', 'Vous avez sauvé Marie Curie');
-insert into achievements (name, description) values ('Tea time', 'Vous avez sauvé Alan Turing');
-insert into achievements (name, description) values ('Mechtulu Ftagh''n', 'Vous avez sauvé Thalès et Pythagore');
-insert into achievements (name, description) values ('Un petit pas pour l''homme', 'Vous avez banni MekAristote');
-insert into achievements (name, description) values ('Victoire !', 'Vous avez sauvé le monde');
-insert into achievements (name, description) values ('Apprenti mathématicien', 'Finir un mini-jeu');
-insert into achievements (name, description) values ('Forever alone', 'Se demander en ami');
-insert into achievements (name, description) values ('Une offre que vous ne pouvez pas refuser', 'Aider la mafyak');
-insert into achievements (name, description) values ('Manifestation spontanée', 'Aider les proloyaks');
-insert into achievements (name, description) values ('Explorateur intrépide', 'Finir un niveau bonus');
-insert into achievements (name, description) values ('Love is real', 'Mettre un mini-jeu en favori');
-insert into achievements (name, description) values ('Philantrope', 'Rejoindre une guilde');
+insert into levels(world, game, x, y, previous) values
+--  7 niveaux pour Mongolie
+	('Mongolie', 1, 0, 0, null),
+	('Mongolie', 1, 0, 0, 1),
+	('Mongolie', 1, 0, 0, 2),
+	('Mongolie', 1, 0, 0, 3),
+	('Mongolie', 1, 0, 0, 4),
+	('Mongolie', 1, 0, 0, 5),
+	('Mongolie', 1, 0, 0, 6),
+-- 6 niveaux pour France
+	('France', 1, 406, 282, null),
+	('France', 1, 952, 226, 8),
+	('France', 1, 1156, 491, 9),
+	('France', 1, 975, 669, 10),
+	('France', 1, 698, 606, 11),
+	('France', 1, 558, 468, 12),
+-- 6 niveaux pour Italie
+	('Italie', 1, 0, 0, null),
+	('Italie', 1, 0, 0, 14),
+	('Italie', 1, 0, 0, 15),
+	('Italie', 1, 0, 0, 16),
+	('Italie', 1, 0, 0, 17),
+	('Italie', 1, 0, 0, 18);
+
+insert into achievements (name, description) values
+	('Un début à tout', 'Créer un compte'),
+	('Chef de guilde', 'Créer une guilde'),
+	('Apprenti sauveur', 'Vous avez sauvé Ada Lovelace'),
+	('Baguette vendéenne', 'Vous avez sauvé François Viète'),
+	('Veni Vidi Zombi', 'Vous avez sauvé Maria Gaëtana Agnési'),
+	('Un curry assez épicé', 'Vous avez sauvé Marie Curie'),
+	('Tea time', 'Vous avez sauvé Alan Turing'),
+	('Mechtulu Ftagh''n', 'Vous avez sauvé Thalès et Pythagore'),
+	('Un petit pas pour l''homme', 'Vous avez banni MekAristote'),
+	('Victoire !', 'Vous avez sauvé le monde'),
+	('Apprenti mathématicien', 'Finir un mini-jeu'),
+	('Forever alone', 'Se demander en ami'),
+	('Une offre que vous ne pouvez pas refuser', 'Aider la mafyak'),
+	('Manifestation spontanée', 'Aider les proloyaks'),
+	('Explorateur intrépide', 'Finir un niveau bonus'),
+	('Love is real', 'Mettre un mini-jeu en favori'),
+	('Philantrope', 'Rejoindre une guilde');
 
