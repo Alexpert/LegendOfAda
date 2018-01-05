483d55c9afe4f4eb66bfcf3ea69848fc6c8b63ef
Valentin Debon <valentin@debon.net>
2018-01-05 14:04:02 +0100

    Renommage dialogues

--- Controller/login.php
+++ Controller/login.php
@@ -9,14 +9,26 @@ if(isset($_POST['username'])
 	and isset($_POST['password'])) {
 	if(isset($_POST['create'])
 		and $_POST['create'] == 'true') {
+		// Méthode du dao pour créer un utilisateur
 		$data['created'] = 'success';
 	}
 
 	$data['username'] = 'Sandrine';
 	$data['avatar'] = 'sandrine.jpg';
 	$data['token'] = random_int(0, PHP_INT_MAX);
+	/*
+	 * $user = (méthode du dao pour se logger)
+	 *
+	 * if($user->token == 0) {
+	 * 	$data['error'] = 'Connexion impossible';
+	 * } else {
+	 * 	$data['username'] = $user->username;
+	 * 	$data['avatar'] = 'sandrine.jpg';
+	 * 	$data['token'] = $user->token;
+	 * }
+	 */
 } else {
-	$data['error'] = 'Connexion impossible';
+	$data['error'] = 'Arguments invalides';
 }
 
 echo json_encode($data);
