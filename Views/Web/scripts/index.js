e53253fe60c4eba8f0baabd0a2fc5e8266a2f975
Valentin Debon <valentin@debon.net>
2018-01-11 17:39:17 +0100

    LES AMIIIIIS

--- Views/Web/scripts/index.js
+++ Views/Web/scripts/index.js
@@ -1,49 +1,28 @@
 
-var queryURL;
-(window.onpopstate = function () {
-    var match,
-        pl     = /\+/g,  // Regex for replacing addition symbol with a space
-        search = /([^&=]+)=?([^&]*)/g,
-        decode = function (s) { return decodeURIComponent(s.replace(pl, " ")); },
-        query  = window.location.search.substring(1);
-
-    queryURL = {};
-    while (match = search.exec(query))
-       queryURL[decode(match[1])] = decode(match[2]);
-})();
-
-function getSession() {
-	return JSON.parse(localStorage.getItem('session'));
-}
-
-function setSession(session) {
-	localStorage.setItem('session', JSON.stringify(session));
-}
-
-function parseResponse(raw) {
-	let response = JSON.parse(raw);
-	console.log(response);
-	if(response.error != undefined) {
-		window.location.assign('login.html');
-	}
-	return response;
-}
-
-function wake() {
+function addFriend() {
 	let session = getSession();
-	var user = document.getElementById('user');
-	var link = document.createElement('a');
-	var text = 'Connexion';
-	var avatar = document.createElement('img');
+	let name = document.getElementById('friendname').value;
+	let requestAdd = new XMLHttpRequest();
 
-	if(session == undefined) {
-		link.setAttribute('href', 'login.html');
-		avatar.setAttribute('src', 'http://api.legendofada.eu/avatar/1.png');
-	} else {
-		link.setAttribute('href', 'profile.html');
-		text = session.username + '  ';
-		avatar.setAttribute('src', 'http://api.legendofada.eu/avatar/' + session.avatar + '.png');
-/*
+	requestAdd.onreadystatechange = function() {
+		if(requestAdd.readyState == 4
+			&& requestAdd.status == 200) {
+			let responseAdd = parseResponse(requestAdd.responseText);
+			if(responseAdd.success == true) {
+				window.location.reload(false);
+			}
+		}
+	}
+
+	requestAdd.open('GET', 'http://api.legendofada.eu/social/friends.php?action=add&token='
+		+ session.token + '&name=' + name);
+	requestAdd.send();
+}
+
+function specific() {
+	let session = getSession();
+
+	if(session != undefined) {
 		let socials = document.getElementsByTagName('social');
 
 		for(var i = 0; i < socials.length; i++) {
@@ -134,14 +113,5 @@ function wake() {
 				requestFriends.send();
 			}
 		}
-		*/
 	}
-
-	link.appendChild(document.createTextNode(text));
-	link.appendChild(avatar);
-
-	user.appendChild(link);
-
-	if(typeof specific == 'function')
-		specific();
 }
